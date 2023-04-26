ef05735763d36ea2b5e2b050571bd3c92d390f31
Shivam1456 <127660326+Shivam1456@users.noreply.github.com>
2023-04-25 23:00:05 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 // your JS code here. If required.
-const gridItems = document.querySelectorAll('.grid-item');
+const gridCells = document.querySelectorAll('.grid-item');
 const blockIdInput = document.getElementById('block_id');
-const colorInput = document.getElementById('colour_id');
+const colorIdInput = document.getElementById('color_id');
 const changeButton = document.getElementById('change_button');
-const resetButton = document.getElementById('reset_button');
-// Function to change the background color of a grid item
-function changeColor(blockId, color) {
-  // Reset all grid items to transparent background color
-  gridItems.forEach(item => item.style.backgroundColor = 'transparent');
-  // Set the background color of the specified grid item to the specified color
-  const block = document.getElementById(blockId);
-  if (block) {
-    block.style.backgroundColor = color;
-  }
-}
+const resetButton = document.getElementById('reset');
 
-// Click event listener for the "Change color" button
 changeButton.addEventListener('click', () => {
   const blockId = blockIdInput.value;
-  const color = colorInput.value;
-  changeColor(blockId, color);
+  const colorId = colorIdInput.value;
+  const selectedCell = document.getElementById(blockId);
+  selectedCell.style.backgroundColor = colorId;
 });
 
-// Click event listener for the "Reset" button
 resetButton.addEventListener('click', () => {
-  // Reset all grid items to transparent background color
-  gridItems.forEach(item => item.style.backgroundColor = 'transparent');
+  gridCells.forEach(cell => {
+    cell.style.backgroundColor = 'transparent';
+  });
 });
+
 
 
